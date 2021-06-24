75333110defe16ec7f3293e8a1b1e92995adfa9b
sonali satapathy <sonalisatapathy1991@gmail.com>
2021-06-24 13:00:40 -0400

    Chenges on products branch

--- shopping-cart/src/component/footer.js
+++ shopping-cart/src/component/footer.js
@@ -3,7 +3,7 @@ import {Container} from 'react-bootstrap';
 
 const Footer = () => {
     const footerCls = {backgroundColor:'#202311',
-    color:'white',
+    color:'#f0f00f',
     textAlign:'center',
     position:'sticky',
     bottom:0
