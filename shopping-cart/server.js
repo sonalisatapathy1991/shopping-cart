36d9ee5e50ce38daff38f96529b2be7f8fc69df9
sonali satapathy <sonalisatapathy1991@gmail.com>
2021-09-13 13:13:15 -0400

    version 1.2.1

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -6,19 +6,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import product from './routes/api/product.js';
 import sizes from './routes/api/sizes.js';
-// if (process.env.NODE_ENV !== 'production') {
-//   import dotenv from 'dotenv';
-//   dotenv.config();
-// }
+const path = require('path');
 
 const app = express();
-//app.use("/", express.static(__dirname + "/public"));
-//app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
-
 app.use(bodyParser.json());
-
-
-
 
 const connectDB = async () => {
   try {
@@ -41,6 +32,19 @@ connectDB();
 
 app.use('/api/products', product);
 app.use('/api/sizes', sizes);
+// if (process.env.NODE_ENV !== 'production') {
+//   import dotenv from 'dotenv';
+//   dotenv.config();
+// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/build/index.html"))
+  }
+  );
+}
+
+
 const port = parseInt(process.env.PORT) || 5000;
 
-app.listen(port, () => console.log(`serve at http://localhost:${port}`));
+app.listen(port, () => console.log(`serve running at :${port}`));
