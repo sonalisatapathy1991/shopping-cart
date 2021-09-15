146663bbce3afc2cbafd2cf6ac414a05da9b18c4
sonali satapathy <sonalisatapathy1991@gmail.com>
2021-09-15 12:52:25 -0400

    server changes

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -38,8 +38,8 @@ app.use('/api/sizes', sizes);
 // }
 //if (process.env.NODE_ENV === 'production') {
 app.use(express.static("/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/build/index.html"))
+app.get("/", (req, res) => {
+  res.send('Hello Express API is Ready !')
 }
 );
 //}
