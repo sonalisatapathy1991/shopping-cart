85461fb890573e148bd28c4d7dbe8b548b8cb5cb
sonali satapathy <sonalisatapathy1991@gmail.com>
2021-09-15 00:19:00 -0400

    package file changes

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -36,18 +36,18 @@ app.use('/api/sizes', sizes);
 //   import dotenv from 'dotenv';
 //   dotenv.config();
 // }
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static("/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "/build/index.html"))
-//   }
-//   );
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/build/index.html"))
+  }
+  );
+}
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'))
-})
+// app.use(express.static(path.join(__dirname, '../build')))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../build'))
+// })
 
 const port = parseInt(process.env.PORT) || 5000;
 
