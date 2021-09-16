acd2ef19b8a47e4648da8a46ece0dd60d7071fe4
sonali satapathy <sonalisatapathy1991@gmail.com>
2021-09-16 13:46:15 -0400

    SErver file

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -37,13 +37,14 @@ app.use('/api/sizes', sizes);
 //   dotenv.config();
 // }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index1.html"))
-  }
-  );
-}
+//if (process.env.NODE_ENV === 'production') {
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  // console.log('Testing----', __dirname)
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+//}
 
 // app.use(express.static(path.join(__dirname, '../build')))
 // app.get('*', (req, res) => {
